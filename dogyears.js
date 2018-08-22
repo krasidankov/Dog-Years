e3963379ef07f70f900f178912a54042e54184aa
Krasimir Dankov <krasidankov@gmail.com>
2018-08-22 16:20:02 +0300

    Initial commit

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+//Human Age
+const myAge = 1
+//
+let earlyYears = 2
+//
+earlyYears *= 10.5;
+//
+let laterYears = myAge - 2;
+//
+laterYears *= 4
+//
+let myAgeInDogYears = earlyYears + laterYears;
+//
+let myName = 'Krasimir Dankov'.toLowerCase();
+console.log('My name is ' + myName + '. I am ' + myAgeInDogYears + ' years old in dog years.');
